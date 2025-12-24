43e74fc9bc9d3f5b24dd54a31f799de04f78432e
龙小芳 <389733290@qq.com>
2025-12-24 17:01:03 +0800

    etst

--- apps/api/models.py
+++ apps/api/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     age = models.IntegerField()
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
